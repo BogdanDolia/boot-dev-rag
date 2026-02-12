093ccf61d9142d669deeaa3361f99f825d57cc15
BogdanDolia <cr.co.erph@gmail.com>
2026-02-12 22:01:54 +0200

    Inverted Index

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,32 +1,43 @@
+"""Keyword Search CLI"""
 #!/usr/bin/env python3
 
+import sys
 import argparse
 import json
-import string
 
-from nltk.stem import PorterStemmer
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+from lib.inverted_index import InvertedIndex
+from lib.text_utils import preprocess
+
+# Add project root to path so `lib` is importable when running script directly
 
 
 def main() -> None:
+    """Main function"""
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
+    subparsers.add_parser("build", help="Build the index")
 
     args = parser.parse_args()
 
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
-            movies = json.load(open("data/movies.json"))
+            movies = json.load(open("data/movies.json", encoding="utf-8"))
             movies_by_id = {movie["id"]: movie for movie in movies["movies"]}
-            with open("data/stopwords.txt") as f:
+            with open("data/stopwords.txt", encoding="utf-8") as f:
                 stop_words = set(f.read().splitlines())
 
             search_query = preprocess(args.query, stop_words)
-            for movie_id, movie in movies_by_id.items():
+            for _, movie in movies_by_id.items():
                 movie_query = preprocess(movie["title"], stop_words)
+                # This is a list comprehension that checks if the search query is in the movie query
+                # and if the length of the search query is greater than 2
                 matches = [
                     s
                     for s in search_query
@@ -35,34 +46,15 @@ def main() -> None:
                 ]
                 if len(matches) > 0:
                     print(f"- '{movie['title']}'")
+        case "build":
+            movies = json.load(open("data/movies.json", encoding="utf-8"))
+            index = InvertedIndex()
+            index.build(movies["movies"])
+            index.save()
+            docs = index.get_documents("merida")
+            print(docs[0])
         case _:
             parser.print_help()
-
-
-def preprocess(text: str, stop_words: set[str]) -> list[str]:
-    normalized = normalize_query(text)
-    tokens = tokenize(normalized)
-    tokens_without_stop_words = remove_stop_words(tokens, stop_words)
-    return stemmer(tokens_without_stop_words)
-
-
-def normalize_query(query: str) -> str:
-    query = query.translate(str.maketrans("", "", string.punctuation))
-    return query.lower()
-
-
-def tokenize(query: str) -> list[str]:
-    return query.split()
-
-
-def remove_stop_words(query: list[str], stop_words: set[str]) -> list[str]:
-    res = [word for word in query if word not in stop_words]
-    return res
-
-
-def stemmer(tokens: list[str]) -> list[str]:
-    stmr = PorterStemmer()
-    return [stmr.stem(token) for token in tokens]
 
 
 if __name__ == "__main__":
